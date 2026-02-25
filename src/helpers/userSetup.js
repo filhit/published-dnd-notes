8e1de5c1089b8d983b0d374e4877713c696054be
filhit <filhit@gmail.com>
2026-02-25 19:49:51 +0100

    Move my changes to user files, so it survives template update

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
@@ -5,6 +6,41 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  const linkFilter = eleventyConfig.getFilter("link");
+
+  eleventyConfig.addFilter("link", function (str) {
+    if (!str) return str;
+
+    return str.replace(/\[\[(.*?)(?:\|(.*?))?\]\]/g, function (match, fileLink, linkTitle) {
+
+      if (fileLink.indexOf("],[") > -1 || fileLink.indexOf('"$"') > -1) {
+        return match;
+      }
+
+      const normalizedLink = linkTitle
+        ? match
+        : `[[${fileLink}|${fileLink}]]`;
+
+      return linkFilter(normalizedLink);
+    });
+  });
+
+  eleventyConfig.on('eleventy.before', async ({ dir }) => {
+    const files = await fs.promises.readdir(dir.input, { recursive: true });
+    const markdownFiles = files.filter((file) => file.endsWith('.md'));
+    for (const file of markdownFiles) {
+      const content = await fs.promises.readFile(`${dir.input}/${file}`, 'utf-8');
+      const frontmatterRegex = /^---([\s\S]*?)---/m;
+      const match = content.match(frontmatterRegex);
+
+      if (match) {
+        const updatedFrontmatter = match[1].replace(/\\\|/g, '|');
+        const updatedContent = content.replace(match[0], `---${updatedFrontmatter}---`);
+        await fs.promises.writeFile(`${dir.input}/${file}`, updatedContent, 'utf-8');
+      }
+    }
+  });
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
